016b88f9931d1a1ed18c975448f1841838ea6c8c
herrfleischladen <herrfleischladen+github@optimail.net>
2025-10-18 00:25:20 +0200

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# external-dx11-overlay-for-nexus
-SorryQuick's "External DX11 Overlay" for the Guild Wars 2 addon manager "Nexus"
+# External DX11 Overlay for Nexus
+POC - Proof Of Concept
---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use nexus::{AddonFlags};
+use windows::Win32::{
+    Foundation::HINSTANCE,
+    System::LibraryLoader::GetModuleHandleW
+};
+
+fn get_hinstance() -> HINSTANCE { unsafe { GetModuleHandleW(None).unwrap().into() } }
+
+fn load_wrapper() {
+    let hinstance = get_hinstance();
+    external_dx11_overlay::attach(hinstance);
+}
+
+fn unload_wrapper() {
+    external_dx11_overlay::detatch();
+}
+
+nexus::export! {
+    name: "External DX11 Overlay for Nexus",
+    signature: -0x7A8B9C2E,
+    load: load_wrapper,
+    unload: unload_wrapper,
+    flags: AddonFlags::None,
+    provider: nexus::UpdateProvider::GitHub,
+    update_link: "https://github.com/herrfleischladen/external-dx11-overlay-for-nexus",
+    log_filter: "trace"
+}
